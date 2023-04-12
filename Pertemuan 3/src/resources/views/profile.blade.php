3cc40fb0999645c93dc4c89197ddde38a185a4d4
NicholasPV <103667899+NicholasPV@users.noreply.github.com>
2023-04-12 13:52:30 +0700

    Pertemuan 3

--- Pertemuan 3/src/resources/views/profile.blade.php
+++ Pertemuan 3/src/resources/views/profile.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.main')
 
 @section('content')
-    <center><img src="https://preview.redd.it/raiden-stickers-from-the-genshin-qq-chat-v0-0pgkt5v0u5m81.jpg?width=390&format=pjpg&auto=webp&s=0ac89430013823ccf17771e05f7342e520c9ae9f" alt="image"></center>
-    
-    <p><center>Nama: Nicholas Patrick Varian</center></p>
-    <p><center>NIM: 20210801102</center></p>
-
-    <p><center>Get Bonked!!!</center></p>
+    <center>
+        <div class="card" style="width: 18rem;">
+            <img src="https://preview.redd.it/raiden-stickers-from-the-genshin-qq-chat-v0-0pgkt5v0u5m81.jpg?width=390&format=pjpg&auto=webp&s=0ac89430013823ccf17771e05f7342e520c9ae9f" alt="image"></center>
+            <div class="card-body">
+                <h5 class="card-title">Nicholas Patrick Varian</h5>
+                <p class="card-text">Get Bonked!!!</p>
+            </div>
+        </div>
+    </center>
 @endsection
